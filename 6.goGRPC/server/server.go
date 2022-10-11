c621ee8cb72704667939dc10284fa6b1ce4909ab
Steve Ng <ntc.steve@outlook.com>
2022-10-11 18:47:53 +0800

    added gRPC

---
+++ 6.goGRPC/server/server.go
@@ -0,0 +1,110 @@
+package main
+
+import (
+	"context"
+	fmt "fmt"
+	"io"
+	"math/rand"
+	"net"
+	"os"
+	"time"
+
+	"github.com/newrelic/go-agent/v3/integrations/nrgrpc"
+	sampleapp "github.com/newrelic/go-agent/v3/integrations/nrgrpc/example/sampleapp"
+	"github.com/newrelic/go-agent/v3/newrelic"
+	"google.golang.org/grpc"
+)
+
+// Server is a gRPC server.
+type Server struct{}
+
+// processMessage processes each incoming Message.
+func processMessage(ctx context.Context, msg *sampleapp.Message) {
+	defer newrelic.FromContext(ctx).StartSegment("processMessage").End()
+	fmt.Printf("Message received: %s\n", msg.Text)
+}
+
+// DoUnaryUnary is a unary request, unary response method.
+func (s *Server) DoUnaryUnary(ctx context.Context, msg *sampleapp.Message) (*sampleapp.Message, error) {
+	processMessage(ctx, msg)
+	randomDelay()
+	return &sampleapp.Message{Text: "Hello from DoUnaryUnary"}, nil
+}
+
+// DoUnaryStream is a unary request, stream response method.
+func (s *Server) DoUnaryStream(msg *sampleapp.Message, stream sampleapp.SampleApplication_DoUnaryStreamServer) error {
+	processMessage(stream.Context(), msg)
+	for i := 0; i < 3; i++ {
+		randomDelay()
+		if err := stream.Send(&sampleapp.Message{Text: "Hello from DoUnaryStream"}); nil != err {
+			return err
+		}
+	}
+	return nil
+}
+
+// DoStreamUnary is a stream request, unary response method.
+func (s *Server) DoStreamUnary(stream sampleapp.SampleApplication_DoStreamUnaryServer) error {
+	for {
+		msg, err := stream.Recv()
+		if err == io.EOF {
+			randomDelay()
+			return stream.SendAndClose(&sampleapp.Message{Text: "Hello from DoStreamUnary"})
+		} else if nil != err {
+			return err
+		}
+		processMessage(stream.Context(), msg)
+	}
+}
+
+// DoStreamStream is a stream request, stream response method.
+func (s *Server) DoStreamStream(stream sampleapp.SampleApplication_DoStreamStreamServer) error {
+	for {
+		msg, err := stream.Recv()
+		if err == io.EOF {
+			return nil
+		} else if nil != err {
+			return err
+		}
+		processMessage(stream.Context(), msg)
+		randomDelay()
+		if err := stream.Send(&sampleapp.Message{Text: "Hello from DoStreamStream"}); nil != err {
+			return err
+		}
+	}
+}
+
+// init sets initial values for variables used in the function.
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+func main() {
+	app, err := newrelic.NewApplication(
+		newrelic.ConfigAppName(os.Getenv("APP_NAME")),
+		newrelic.ConfigLicense(os.Getenv("NEW_RELIC_LICENSE_KEY")),
+		// newrelic.ConfigDebugLogger(os.Stdout),
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	lis, err := net.Listen("tcp", "localhost:8080")
+	if err != nil {
+		panic(err)
+	}
+
+	// Add the New Relic gRPC server instrumentation
+	grpcServer := grpc.NewServer(
+		grpc.UnaryInterceptor(nrgrpc.UnaryServerInterceptor(app)),
+		grpc.StreamInterceptor(nrgrpc.StreamServerInterceptor(app)),
+	)
+	sampleapp.RegisterSampleApplicationServer(grpcServer, &Server{})
+	grpcServer.Serve(lis)
+
+}
+
+func randomDelay() {
+	r := rand.Intn(500)
+	time.Sleep(time.Duration(r) * time.Millisecond)
+}
